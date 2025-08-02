080a1af450be6a6d150b3b5d51152d5b1636a71b
Sai Kiran <ksai41530@gmail.com>
2025-08-02 10:15:00 +0000

    Remove temporary build and test scripts

--- update_dates_filter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Update commit dates from 2024 to 2025
-if [ -n "$GIT_AUTHOR_DATE" ]; then
-  export GIT_AUTHOR_DATE="${GIT_AUTHOR_DATE//2024/2025}"
-fi
-if [ -n "$GIT_COMMITTER_DATE" ]; then
-  export GIT_COMMITTER_DATE="${GIT_COMMITTER_DATE//2024/2025}"
-fi
